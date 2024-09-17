aac375b4767254cc373ecf72feca9b8009732b55
shawnlin <linshi584@gmail.com>
2024-09-17 12:39:22 +0800

    update simple1

---
+++ udemy_python_all/coding_practice/intermediate_1.py
@@ -0,0 +1,42 @@
+
+
+# -----------------4
+# def pyramid(num):
+#     for n in range(1, num+1):
+#         print(' ' * (num - n) + '*' * (2*n-1))
+
+
+# pyramid(1)
+# print('---------')
+# pyramid(2)
+# print('---------')
+# pyramid(4)
+# print('---------')
+
+# -----------------5
+# def inversePyramid(num):
+#     for n in range(num, 0, -1):
+#         print(' ' * (num-n) + '*' * (2*n-1))
+
+
+# inversePyramid(4)
+# print('---------')
+# inversePyramid(3)
+# print('---------')
+# inversePyramid(2)
+# print('---------')
+
+# -----------------6
+# def has_33(list1):
+#     for n in range(len(list1)):
+#         if (list1[n] == 3) and (list1[n+1] == 3):
+#             return True
+#     return False
+
+
+# print(has_33([1, 2, 3, 4, 5, 3, 3]))
+# print(has_33([]))
+# print(has_33([4, 3, 4, 5, 2, 1]))
+
+
+# -----------------7
